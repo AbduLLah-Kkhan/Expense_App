070b8c64ddf728736d2a53be8e6d1b53c3e500ee
AbduLLah-Kkhan <abdullah.epost@gmail.com>
2024-08-02 20:00:52 +0500

    rearrange db

--- Expense_app.sql
+++ Expense_app.sql
@@ -1,35 +1,49 @@
+drop table Income;
+drop table Users;
+drop table Expenses;
+drop table Sender;
 create schema Expense_app;
+
 
 use Expense_app;
 
-
-create table Users ( 
-	user_id int auto_increment	primary key,
-    user_name varchar (255) not null
+create table Users (
+	user_id int auto_increment primary key,
+    user_name varchar(255),
+    user_email varchar(255) unique
 );
+
 
 create table Expenses ( 
 	expense_id int auto_increment primary key,
     user_id int,
-    Item_name varchar (255),
-    Amount decimal (10,2),
+    item_name varchar (255),
+	item_quantity int,
+    item_price decimal (10,2),
+    total_amount decimal (10,2),
     date DATE not null,
     foreign key (user_id) references Users(user_id)
 );
 
+-- total sum etc, total amount, remaining amount can be retrieved from income and expense table
 CREATE TABLE Income (
     Income_id INT AUTO_INCREMENT PRIMARY KEY,
-    Sender_name VARCHAR(255),
+	user_id int,
+	sender_id int,
     Amount DECIMAL(10,2),
-    date DATE NOT NULL
+    date DATE NOT NULL,
+    foreign key (user_id) references Users(user_id),
+	foreign key (sender_id) references Sender(sender_id)
 );
 
+-- Sender seperate table. (Multiple senders possible)
+-- sender's foriegn key in income, as multiple amount possible
 
-create table Balance (
-	balance_id int auto_increment primary key,
-    Total_Income decimal (10,2),
-    Current_amount decimal (10,2),
-    Expense decimal (10,2),
-    Remaining_amount decimal (10,2)
-);
+create table Sender (
+	sender_id int auto_increment primary key,
+    sender_name varchar(255)
+)
+
+
+
 
